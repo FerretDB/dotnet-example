da3feaa3f0bba5039087a26f5c2d806103bdd7ed
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2023-05-11 18:48:08 +0400

    Re-add project file to make it easier to run (#2)

--- Program.cs
+++ Program.cs
@@ -1,11 +1,4 @@
-﻿// how to run:
-// 1. install dotnet runtime https://learn.microsoft.com/en-us/dotnet/core/install/linux-scripted-manual#scripted-install
-// 2. export DOTNET_ROOT=$HOME/.dotnet
-// 3. export PATH=$PATH:$HOME/.dotnet:$HOME/.dotnet/tools
-// 4. dotnet add package MongoDB.Driver, if you cannot add the package you may have to first run `dotnet new console --force`
-// 5. dotnet run
-
-using MongoDB.Driver;
+﻿using MongoDB.Driver;
 using MongoDB.Bson;
 
 const string connectionUri = "mongodb://localhost:27017/?directConnection=true";
