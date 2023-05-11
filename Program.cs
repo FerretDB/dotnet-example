1adcf18efd562fcb9cfea201c48bcbeefb24e035
b1ron <80292536+b1ron@users.noreply.github.com>
2023-05-11 16:37:19 +0200

    Use a minimal repro (#1)

--- Program.cs
+++ Program.cs
@@ -1,26 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+﻿// how to run:
+// 1. install dotnet runtime https://learn.microsoft.com/en-us/dotnet/core/install/linux-scripted-manual#scripted-install
+// 2. export DOTNET_ROOT=$HOME/.dotnet
+// 3. export PATH=$PATH:$HOME/.dotnet:$HOME/.dotnet/tools
+// 4. dotnet add package MongoDB.Driver, if you cannot add the package you may have to first run `dotnet new console --force`
+// 5. dotnet run
 
-namespace mongodb_dotnet_example
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+using MongoDB.Driver;
+using MongoDB.Bson;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+const string connectionUri = "mongodb://localhost:27017/?directConnection=true";
+
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+var client = new MongoClient(connectionUri);
+
+var collection = client.GetDatabase("test").GetCollection<BsonDocument>("foo");
+
+var filter = Builders<BsonDocument>.Filter.Eq("name", "foo");
+
+var res = collection.Find(filter).First();
+
+Console.WriteLine(res);
