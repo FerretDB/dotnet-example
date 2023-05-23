cab92f0bf33381d3ad1036ee19a6e33bdec3e733
b1ron <80292536+b1ron@users.noreply.github.com>
2023-05-23 11:55:58 +0200

    Make various preparations for CI integration (#3)

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
 ﻿using MongoDB.Driver;
 using MongoDB.Bson;
 
-const string connectionUri = "mongodb://localhost:27017/?directConnection=true";
-
-var settings = MongoClientSettings.FromConnectionString(connectionUri);
+const string connectionUri = "mongodb://localhost:27017/";
 
 var client = new MongoClient(connectionUri);
 
@@ -11,6 +9,4 @@ var collection = client.GetDatabase("test").GetCollection<BsonDocument>("foo");
 
 var filter = Builders<BsonDocument>.Filter.Eq("name", "foo");
 
-var res = collection.Find(filter).First();
-
-Console.WriteLine(res);
+_ = collection.Find(filter).FirstOrDefault();
