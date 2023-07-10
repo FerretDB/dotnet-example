6043c71e2970d70727a6769f94524ebd927d1de8
b1ron <goldingbyron@gmail.com>
2023-07-10 21:03:24 +0200

    tidy

--- Program.cs
+++ Program.cs
@@ -5,19 +5,17 @@ public static class Example
 {
     public static void Main(string[] args)
     {
-        foreach (Object obj in args) {
-            var settings = MongoClientSettings.FromConnectionString(obj.ToString());
-            // https://jira.mongodb.org/browse/CSHARP-3516
-            // Set the ServerApi field of the settings object to Stable API version 1
-            // to force OP_MSG to be used in hello command during handshake.
-            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-            var client = new MongoClient(settings);
+        var settings = MongoClientSettings.FromConnectionString(args[0].ToString());
+        // https://jira.mongodb.org/browse/CSHARP-3516
+        // Set the ServerApi field of the settings object to Stable API version 1
+        // to force OP_MSG to be used in hello command during handshake.
+        settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+        var client = new MongoClient(settings);
 
-            IMongoDatabase db = client.GetDatabase("test");
-            var command = new BsonDocument { { "ping", 1 } };
-            var res = db.RunCommand<BsonDocument>(command);
+        IMongoDatabase db = client.GetDatabase("test");
+        var command = new BsonDocument { { "ping", 1 } };
+        var res = db.RunCommand<BsonDocument>(command);
 
-            Console.WriteLine(res);
-        }
+        Console.WriteLine(res);
     }
 }
