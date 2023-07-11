69ea4e2400c6f89f6827fae2ab6958ae2bf7dc9d
b1ron <goldingbyron@gmail.com>
2023-07-11 18:15:51 +0200

    revert setting API version

--- Program.cs
+++ Program.cs
@@ -5,12 +5,7 @@ public static class Example
 {
     public static void Main(string[] args)
     {
-        var settings = MongoClientSettings.FromConnectionString(args[0].ToString());
-        // https://jira.mongodb.org/browse/CSHARP-3516
-        // Set the ServerApi field of the settings object to Stable API version 1
-        // to force OP_MSG to be used in hello command during handshake.
-        settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-        var client = new MongoClient(settings);
+        var client = new MongoClient(args[0].ToString());
 
         IMongoDatabase db = client.GetDatabase("test");
         var command = new BsonDocument { { "ping", 1 } };
